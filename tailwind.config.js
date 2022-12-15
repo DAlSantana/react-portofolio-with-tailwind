ed8b1f88eda2579925ab6037a5960f546f8ec7ed
Diego Santana <diegosantana@MacBook-Air-de-Diego.local>
2022-12-15 18:00:38 -0300

    Add de pagina de portifolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,20 @@ module.exports = {
         burtons: "burtons",
         poppins: ["Poppins", "sans-serif"],
       },
+      keyframes: {
+              hello: {
+                "0%": {
+                  transform: "rotate(0deg)",
+                },
+                "100%": {
+                  transform: "rotate(3600deg)",
+                },
+              },
+            },
+            animation: {
+              rotate: "hello 70.31s  infinite",
+            },
+      },
     },
-  },
   plugins: [],
 };
